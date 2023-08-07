0345d2217ffd4f1c0a90a433ce9baf529368a08d
NithinSai0110 <113330780+NithinSai0110@users.noreply.github.com>
2023-08-07 10:15:10 +0530

    Update fods Q37.py

--- fods Q37.py
+++ fods Q37.py
@@ -2,17 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-# Step 2: Load and Prepare Data
 data = pd.read_csv("csv37.csv")
 
-# Step 3: Explore the Data
 print(data.head())
 print(data.describe())
 
-# Step 4: Visualize the Data
-
-# Scatter Plot
 plt.figure(figsize=(10, 6))
 plt.scatter(data['study_time'], data['exam_score'])
 plt.title('Study Time vs Exam Score')
@@ -20,7 +14,6 @@ plt.xlabel('Study Time (hours)')
 plt.ylabel('Exam Score')
 plt.show()
 
-# Regression Plot
 plt.figure(figsize=(10, 6))
 sns.regplot(x='study_time', y='exam_score', data=data)
 plt.title('Study Time vs Exam Score')
@@ -28,7 +21,6 @@ plt.xlabel('Study Time (hours)')
 plt.ylabel('Exam Score')
 plt.show()
 
-# Correlation Heatmap
 correlation_matrix = data.corr()
 plt.figure(figsize=(8, 6))
 sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')

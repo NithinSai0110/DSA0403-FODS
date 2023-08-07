85e181cd761b201d32062eb9c1a83404e9207787
NithinSai0110 <113330780+NithinSai0110@users.noreply.github.com>
2023-08-07 10:16:51 +0530

    Update fods Q11 barplot.py

--- fods Q11 barplot.py
+++ fods Q11 barplot.py
@@ -1,16 +1,14 @@
 import matplotlib.pyplot as plt
 
-# Sample data (replace this with your actual data)
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 sales_values = [1000, 1200, 800, 1500, 1400, 1600, 1100, 1300, 1700, 1900, 2000, 1800]
 
-# Create a bar plot
-plt.figure(figsize=(8, 6))  # Set the figure size
+plt.figure(figsize=(8, 6)) 
 plt.bar(months, sales_values, color='g')
 plt.title('Monthly Sales Data')
 plt.xlabel('Month')
 plt.ylabel('Sales')
-plt.grid(True)  # Show gridlines
-plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
-plt.tight_layout()  # Adjust layout to prevent overlapping
+plt.grid(True) 
+plt.xticks(rotation=45) 
+plt.tight_layout() 
 plt.show()
